23b7c5a317cbf7348386a9df0270339d41700fee
Chris Gosser <rivetkopf@gmail.com>
2018-11-16 12:29:54 -0800

    add line breaks between the paragraphs in the articles section

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -13,11 +13,14 @@ function MainContent(){
       <h3>DE IMPERIO CN. POMPEI AD QVIRITES ORATIO</h3><hr/>
       <p>
     [1] Quamquam mihi semper frequens conspectus vester multo iucundissimus, hic autem locus ad agendum amplissimus, ad dicendum ornatissimus est visus, Quirites, tamen hoc aditu laudis, qui semper optimo cuique maxime patuit, non mea me voluntas adhuc, sed vitae meae rationes ab ineunte aetate susceptae prohibuerunt. Nam cum antea per aetatem nondum huius auctoritatem loci attingere auderem, statueremque nihil huc nisi perfectum ingenio, elaboratum industria adferri oportere, omne meum tempus amicorum temporibus transmittendum putavi.
-
+    <br/>
+    <br/>
     [2] Ita neque hic locus vacuus umquam fuit ab eis qui vestram causam defenderent, et meus labor, in privatorum periculis caste integreque versatus, ex vestro iudicio fructum est amplissimum consecutus. Nam cum propter dilationem comitiorum ter praetor primus centuriis cunctis renuntiatus sum, facile intellexi, Quirites, et quid de me iudicaretis, et quid aliis praescriberetis. Nunc cum et auctoritatis in me tantum sit, quantum vos honoribus mandandis esse voluistis, et ad agendum facultatis tantum, quantum homini vigilanti ex forensi usu prope cotidiana dicendi exercitatio potuit adferre, certe et si quid auctoritatis in me est, apud eos utar qui eam mihi dederunt, et si quid in dicendo consequi possum, eis ostendam potissimum, qui ei quoque rei fructum suo iudicio tribuendum esse duxerunt.
-
+    <br/>
+    <br/>
     [3] Atque illud in primis mihi laetandum iure esse video, quod in hac insolita mihi ex hoc loco ratione dicendi causa talis oblata est, in qua oratio deesse nemini possit. Dicendum est enim de Cn. Pompei singulari eximiaque virtute: huius autem orationis difficilius est exitum quam principium invenire. Ita mihi non tam copia quam modus in dicendo quaerendus est.
-
+    <br/>
+    <br/>
     [4] Atque,—ut inde oratio mea proficiscatur, unde haec omnis causa ducitur,—bellum grave et periculosum vestris vectigalibus ac sociis a duobus potentissimis regibus infertur, Mithridate et Tigrane, quorum alter relictus, alter lacessitus, occasionem sibi ad occupandam Asiam oblatam esse arbitrantur. Equitibus Romanis, honestissimis viris, adferuntur ex Asia cotidie litterae, quorum magnae res aguntur in vestris vectigalibus exercendis occupatae: qui ad me, pro necessitudine quae mihi est cum illo ordine, causam rei publicae periculaque rerum suarum detulerunt:
       </p>
     </div>
